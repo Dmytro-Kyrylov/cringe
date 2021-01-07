13dad04f8558d606d9cb3d481a1f7b1ec6259a98
KyrylovDV <d.kyrylov@softarex.com>
2021-01-07 18:33:04 +0200

    Enable CORS. Finally fixed

--- src/main/java/friends/cringe/common/config/SecurityConfig.java
+++ src/main/java/friends/cringe/common/config/SecurityConfig.java
@@ -23,8 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .csrf().disable()
-        .cors().disable()
         .authorizeRequests().anyRequest().authenticated()
         .and().httpBasic();
   }
